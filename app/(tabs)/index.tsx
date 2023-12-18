0a90d5ea4ea0ac33bcbc6aaf9be754ea9f570053
stanleyos <stanislaw.sarka@gmail.com>
2023-12-18 11:34:10 +0100

    Added Reanimated and created first animation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,34 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Button } from "react-native";
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import EditScreenInfo from "../../components/EditScreenInfo";
+import { Text, View } from "../../components/Themed";
+import Animated, { useSharedValue, withSpring } from "react-native-reanimated";
 
 export default function TabOneScreen() {
+  const width = useSharedValue(100);
+
+  const handlePress = () => {
+    width.value = withSpring(width.value + 50);
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Animated.View
+        style={{
+          width,
+          height: 100,
+          backgroundColor: "violet",
+          borderRadius: 10,
+        }}
+      />
+      <Button onPress={handlePress} title="Click me" />
     </View>
   );
 }
@@ -16,16 +36,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
